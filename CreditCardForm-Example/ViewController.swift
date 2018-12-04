2a1910b1ba927d4637e0667f2d177f5ca56004af
Apple <apple@apples-macbook-pro.local>
2018-12-04 15:55:04 +0330

    Initial Commit

---
+++ CreditCardForm-Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CreditCardForm-Example
+//
+//  Created by Apple on 12/4/18.
+//  Copyright © 2018 Amir Farsad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CreditCardForm-ExampleTests/CreditCardForm_ExampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  CreditCardForm_ExampleTests.swift
+//  CreditCardForm-ExampleTests
+//
+//  Created by Apple on 12/4/18.
+//  Copyright © 2018 Amir Farsad. All rights reserved.
+//
+
+import XCTest
+@testable import CreditCardForm_Example
+
+class CreditCardForm_ExampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
